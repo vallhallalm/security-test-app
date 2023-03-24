cf80e8826f685b0224d9a6cca5db057b500697b5
louis <louis.magnier@gmail.com>
2023-03-24 19:01:27 +0100

    Create two endpoint to authenticate user and to add a nex user to the db

--- Unsecured/backendJavascript/routes/index.js
+++ Unsecured/backendJavascript/routes/index.js
@@ -1,9 +1,55 @@
 var express = require('express');
 var router = express.Router();
+const mysql = require('mysql');
+const conf = require ('../conf.json')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/:id/:pwd', function(req, res) {
+  let connection = mysql.createConnection({
+    host: conf.DB_HOST,
+    port: conf.DB_PORT,
+    user: 'root',
+    password: 'root',
+    database: 'projetIF27'
+  });
+
+  connection.query(`SELECT * FROM user WHERE username='${req.params.id}' && password='${req.params.pwd}'`, (err, result) => {
+    if(err) {
+      res.send(err)
+    } else {
+      console.log(`SELECT * FROM user WHERE username='${req.params.id}' && password='${req.params.pwd}'` , result)
+      if(result.length!==0) {
+        res.send({"auth":true})
+      } else {
+        res.send({"auth":false})
+      }
+    }
+  })
+
+  connection.end()
+});
+
+router.post('/:id/:pwd', function(req, res) {
+  let connection = mysql.createConnection({
+    host: conf.DB_HOST,
+    port: conf.DB_PORT,
+    user: 'root',
+    password: 'root',
+    database: 'projetIF27'
+  });
+
+  connection.query(`INSERT INTO user(username, password) VALUES ('${req.params.id}', '${req.params.pwd}')`, (err, result) => {
+    if(err) {
+      res.send(err)
+    } else {
+      res.send({
+        "status": 201,
+      });
+      res.status(201).end()
+    }
+  })
+
+  connection.end()
 });
 
 module.exports = router;
