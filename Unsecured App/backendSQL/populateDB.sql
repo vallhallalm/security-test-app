f76030091c6858fc2071b4a8d20e4c9286fd9ba3
louis <louis.magnier@gmail.com>
2023-03-24 17:04:35 +0100

    Add SQL backend to project

---
+++ Unsecured App/backendSQL/populateDB.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE projetIF27;
+USE projetIF27;
+CREATE TABLE user (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL
+);
+INSERT INTO user (id, username, password) values (1, "louis", "123"), (2, "vincent", "azerty");
